bda6f0c5a55082c0ff708ff9acd3390c805adb93
Elvrt <elvarettas23@gmail.com>
2024-03-16 14:05:50 +0700

    Praktikum 2.4 – Retreiving or Creating Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,23 +10,15 @@ class UserController extends Controller
 {
     public function index(){
 
-        // $user = UserModel::find(1);
-        // $user = UserModel::where('level_id', 1) -> first();
-        // $user = UserModel::firstWhere('level_id', 1);
-
-        // $user = UserModel::findOr(1,['username','nama'], function () {
-        //     abort(404);
-        // });
-
-        // $user = UserModel::findOr(20,['username','nama'], function () {
-        //     abort(404);
-        // });
-
-        // $user = UserModel::findOrFail(1);
-
-        // $user = UserModel::where('username', 'manager9')->firstOrFail();
-
-        $user = UserModel::where('level_id', 2)->count();
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id'=> 2
+            ],
+         );
+         $user->save();
         return view('user',['data' => $user]);
     }
 }
