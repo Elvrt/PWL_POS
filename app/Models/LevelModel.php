6212908a62d2d9b86d899390a354e1065cdb877d
Elvrt <elvarettas23@gmail.com>
2024-03-16 16:30:34 +0700

    Praktikum 2.7 – Relationships

---
+++ app/Models/LevelModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class LevelModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'm_level'; // Nama tabel yang digunakan untuk model ini
+    protected $primaryKey = 'level_id'; // Nama primary key pada tabel
+
+    protected $fillable = ['level_id', 'level_kode', 'level_nama'];
+
+}
