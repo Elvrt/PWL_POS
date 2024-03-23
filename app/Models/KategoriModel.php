457c3e5b1bad1aa04678006ac0e04df2f729cc80
Elvrt <elvarettas23@gmail.com>
2024-03-23 15:10:24 +0700

    Praktikum 3 – Membuat form kemudian menyimpan data dalam database

--- app/Models/KategoriModel.php
+++ app/Models/KategoriModel.php
@@ -10,6 +10,8 @@ class KategoriModel extends Model
 {
     protected $table = 'm_kategori';
     protected $primaryKey = 'kategori_id';
+
+    protected $fillable = ['kategori_kode', 'kategori_nama'];
     
     public function barang(): HasMany
     {
